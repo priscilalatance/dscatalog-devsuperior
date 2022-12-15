615e1133dda48ee19e5f430ded7cffc78e58a90b
Priscila Padilla Latance <priscila.latance@gmail.com>
2022-12-15 10:46:20 -0300

    Database Exception

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -64,7 +64,7 @@ public class CategoryService {
 			repository.deleteById(id);			
 		}
 		catch(EmptyResultDataAccessException e) {
-			throw new ResourceNotFoundException("Id not found" + id);
+			throw new ResourceNotFoundException("Id not found " + id);
 		}
 		catch(DataIntegrityViolationException e) {
 			throw new DataBaseException("Integrity violation");
